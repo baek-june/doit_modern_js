bdbdb0c850aa89ce9107854b24c07e6ec9999dd4
100june <100june@100juneui-MacBookAir.local>
2023-12-12 12:16:15 +0900

    variable

---
+++ chapter_02/varable_1.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+  return a + b;
+}
+
+var sum = add(10, 20);
+console.log(sum);
+var sum = 100;
+console.log(sum);
---
+++ chapter_02/varable_2.js
@@ -0,0 +1,8 @@
+const myNumber = 10;
+
+function add(a, b) {
+  return a + b;
+}
+
+let sum = add(10, 20);
+console.log(sum);
