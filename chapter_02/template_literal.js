67f77044b0a2b6377b37704432e7b254c8b2f340
100june <100june@100juneui-MacBookAir.local>
2023-12-13 19:12:15 +0900

    template literal

---
+++ chapter_02/template_literal.js
@@ -0,0 +1,7 @@
+let name = "도레미";
+let classroom = 201;
+console.log(name + "님. " + classroom + "호 강의실로 입장하세요.");
+
+name = "백두산";
+classroom = 205;
+console.log(`${name}님, ${classroom}호 강의실로 입장하세요.`);
---
+++ chapter_02/typeof.js
@@ -0,0 +1,17 @@
+let data = 5;
+
+console.log("==== 숫자 =====");
+console.log(typeof(data));
+console.log(typeof(10));
+console.log(typeof("10"));
+console.log(typeof(3.145));
+console.log(0.1 + 0.2);
+
+console.log("==== 문자 =====");
+console.log(typeof("안녕하세요"));
+console.log(typeof('10'));
+console.log(typeof("10"));
+console.log(typeof(""));
+
+console.log('I\'m studying now.');
+console.log('탭 \t 포함');
